067e348ea51290bbfddd960b022b577e426eb5dc
Eric Newton <ericnewton76@gmail.com>
2017-12-14 15:22:13 -0500

    initial commit of project files

---
+++ src/cpcontrib.mocks/MockSmtpImportContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrownPeak.CMSAPI;
+
+/* Some Namespaces are not allowed. */
+namespace CrownPeak.CMSAPI.CustomLibrary
+{
+	public class MockSmtpImportContext
+	{
+		public List<MockEmailAttachment> EmailAttachments = new List<MockEmailAttachment>();
+		public string Body;
+		public bool AutoCreateAsset; public string AssetLabel { get; set; }
+		public string From;
+		public MockEmailAttachment AddEmailAttachment(MockEmailAttachment e)
+		{
+			this.EmailAttachments.Add(e);
+			return e;
+		}
+		public void SetBody(params string[] args)
+		{
+			this.Body = string.Join("\n", args);
+		}
+	}
+	public class MockEmailAttachment
+	{
+		public string StringValue;
+		public string Label;
+	}
+}
